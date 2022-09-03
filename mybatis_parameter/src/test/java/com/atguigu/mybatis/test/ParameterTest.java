e1699ec97083fcf4e544d03becb102f87752581a
Rollors <18270387673@163.com>
2022-09-03 15:32:17 +0800

    mybatis参数获取5种方式

--- mybatis_parameter/src/test/java/com/atguigu/mybatis/test/ParameterTest.java
+++ mybatis_parameter/src/test/java/com/atguigu/mybatis/test/ParameterTest.java
@@ -6,6 +6,9 @@ import com.atguigu.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParameterTest {
 
     @Test
@@ -13,6 +16,41 @@ public class ParameterTest {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         User user = mapper.getUserByUserName("root");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLogin(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLogin("root","123");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLoginByMap(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("username","root");
+        map.put("password","123");
+        User user = mapper.checkLoginByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User(null,"root1","123456",33,"女","12345@qq.com");
+        mapper.insertUser(user);
+    }
+
+    @Test
+    public void testCheckLoginByParam(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLoginByParam("root1","123456");
         System.out.println(user);
     }
 }
