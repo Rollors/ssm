4b572fd197bec0c1c7c46bac0431b300802b3628
Rollors <18270387673@163.com>
2022-09-07 19:02:10 +0800

    mybatis的属性名与字段名不一致情况处理

--- mybatis_resultMap/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
+++ mybatis_resultMap/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
@@ -1,4 +1,13 @@
 package com.atguigu.mybatis.mapper;
 
+import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 public interface EmpMapper {
+    /**
+     * 根据id来查询员工信息
+     * @param empId
+     * @return
+     */
+    Emp getEmpByEmpId(@Param("empId") Integer empId);
 }
