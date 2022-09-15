9d849b320d755e76508482638267263925a9fa1c
Rollors <18270387673@163.com>
2022-09-15 20:06:25 +0800

    mybatis的延时加载

--- mybatis_resultMap/src/test/java/ResultMapTest.java
+++ mybatis_resultMap/src/test/java/ResultMapTest.java
@@ -26,6 +26,6 @@ public class ResultMapTest {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
         Emp emp = mapper.getEmpAndDeptByEmpIdStepOne(1);
-        System.out.println(emp);
+        System.out.println(emp.getEmpName());
     }
 }
